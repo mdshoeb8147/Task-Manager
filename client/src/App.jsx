a480e01617485533082ebcf33d965c292f6e78e1
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-07-23 22:00:40 +0530

    routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
+import Trash from "./pages/Trash";
+import TaskDetails from "./pages/TaskDetails";
+import Toaster from "sonner";
 
 function App() {
   return (
@@ -10,8 +15,17 @@ function App() {
           <Route path="/" element={<Navigate to="/dashboard" />} />
 
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
         </Route>
+        <Route path="/log-in" element={<Login />} />
       </Routes>
+      <Toaster richColors />
     </main>
   );
 }
