5f4470c48c3ff2417d19909646762231af2a04a6
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-07-25 23:14:15 +0530

    complete right side

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -29,6 +29,13 @@ const Login = () => {
             <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600 ">
               Manage all your Task....
             </span>
+            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700 ">
+              <span>Cloud-Based</span>
+              <span>Task Manager</span>
+            </p>
+            <div className="cell">
+              <div className="circle rotate-in-up-left"></div>
+            </div>
           </div>
         </div>
         {/* right side */}
