37827e41a3073605c63877d2a1d3691af10efaa9
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-07-25 22:16:18 +0530

    login logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,25 @@
+import { useForm } from "react-hook-form";
+
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const Login = () => {
+  const user = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  useEffect(
+    () => {
+      user && navigate("/dashboard");
+    },
+    { user }
+  );
+
   return <h1>login</h1>;
 };
 export default Login;
