8e18e0ac19c68a5e01e8306181998bf6ffbe7fe6
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-07-31 21:03:07 +0530

    to do 4440

---
+++ client/src/components/Textbox.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import clsx from "clsx";
+
+const Textbox = React.forwardRef(
+  ({ type, placeholder, label, className, register, name, error }, ref) => {
+    return <div className=""></div>;
+  }
+);
+export default Textbox;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import Textbox from "../components/Textbox";
 
 const Login = () => {
   const user = "";
