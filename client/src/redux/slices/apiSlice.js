8b0f4a22caf89f4d38641796198f1d2baa079193
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-08-05 23:15:42 +0530

    error resolved for store.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:5000/api";
+const API_URI = "http://localhost:8800/api";
 
-const BaseQuery = fetchBaseQuery({ baseUrl: API_URI });
+const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
 export const apiSlice = createApi({
-  BaseQuery,
+  baseQuery,
   tagTypes: [],
-  endPoints: (builder) => ({}),
+  endpoints: (builder) => ({}),
 });
