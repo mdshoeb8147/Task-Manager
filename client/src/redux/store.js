6aa92f50864c14378f270f581d439831601f7ebf
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-08-05 22:50:48 +0530

    store started

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,13 @@
-import {configureStore} from "@reduxjs/toolkit"
-import authSlice from "./slices/authSlice"
-import {apiSlice} from './slices/apiSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-
-    reduce : {
-        [apiSlice,reducerPath]: apiSlice.reducer,
-        auth:authReducer,
-    },
-    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-devTools:true,
+  reduce: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
